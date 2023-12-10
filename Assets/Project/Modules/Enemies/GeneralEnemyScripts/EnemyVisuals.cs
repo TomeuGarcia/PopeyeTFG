cd3c3c4e5527d61baedb43ad298016d829eb5131
Eric <eric.ruiz@enti.cat>
2023-12-10 19:15:44 +0100

    Visual effects general configuration needs to be moved to scriptable

--- Assets/Project/Modules/Enemies/GeneralEnemyScripts/EnemyVisuals.cs
+++ Assets/Project/Modules/Enemies/GeneralEnemyScripts/EnemyVisuals.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public class EnemyVisuals : MonoBehaviour
@@ -8,18 +10,47 @@ public class EnemyVisuals : MonoBehaviour
     //effects must be done by creating a new unique script
     //that inherits from this one.
     
-    private struct MaterialFlash
+    [System.Serializable]
+    public class MaterialFlash
     {
-        [SerializeField] private Material _flashMaterial;
-        [SerializeField] private float _waitTime;
+        public Material _flashMaterial;
+        public float _waitTime;
+    }
+    
+    [System.Serializable]
+    public class OriginalMeshData
+    {
+        public MeshRenderer _mesh;
+        [HideInInspector] public Material[] _originalMaterials;
     }
 
-    [SerializeField] private List<Material> _originalMaterials;
-    [SerializeField] private List<MaterialFlash> _flashSequence;
-    
+    [SerializeField] private List<OriginalMeshData> _originalMeshDatas = new();
+    [SerializeField] private List<MaterialFlash> _flashSequence = new();
+
+    private void OnValidate()
+    {
+        foreach (var data in _originalMeshDatas)
+        {
+            data._originalMaterials = data._mesh.sharedMaterials;
+        }
+    }
+
     public virtual void OnHitEffects()
     {
         
+    }
+    
+    private async UniTaskVoid WaitExample() 
+    {
+        foreach (var flash in _flashSequence)
+        {
+            foreach (var data in _originalMeshDatas)
+            {
+                
+            }
+            
+            //await UniTask.Delay(TimeSpan.FromSeconds(secondsToWait));
+        }
     }
     
     public virtual void OnDeathEffects()
