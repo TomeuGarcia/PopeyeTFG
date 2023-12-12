858b29f01efcde1c730fdc6c4a56377438d51863
Eric <eric.ruiz@enti.cat>
2023-12-12 20:20:13 +0100

    Merge done

--- Assets/Project/Modules/Enemies/Slime/Scripts/SlimeRefactor/EnemyHealth.cs
+++ Assets/Project/Modules/Enemies/Slime/Scripts/SlimeRefactor/EnemyHealth.cs
@@ -9,7 +9,7 @@ namespace Popeye.Modules.Enemies.Components
     public class EnemyHealth : MonoBehaviour, IDamageHitTarget
     {
         private HealthSystem _healthSystem;
-        [SerializeField, Range(0.0f, 100.0f)] private float _maxHealth = 50.0f;
+        [SerializeField, Range(0, 100)] private int _maxHealth = 50;
         [SerializeField] private EnemyVisuals _enemyVisuals;
 
         private IEnemyMediator _mediator;
@@ -32,7 +32,7 @@ namespace Popeye.Modules.Enemies.Components
 
         public DamageHitResult TakeHitDamage(DamageHit damageHit)
         {
-            _healthSystem.TakeDamage(damageHit.Damage);
+            int receivedDamage = _healthSystem.TakeDamage(damageHit.Damage);
             
             if (IsDead())
             {
