a8118f5819d7bf39eda8dc12b204b7e5f2bd5fbb
Eric <eric.ruiz@enti.cat>
2023-12-17 23:45:37 +0100

    Effect done & shadows fixed

--- Assets/Project/Modules/Enemies/GeneralEnemyScripts/VFXRelated/GeneralEnemyVFXConfig.cs
+++ Assets/Project/Modules/Enemies/GeneralEnemyScripts/VFXRelated/GeneralEnemyVFXConfig.cs
@@ -16,6 +16,25 @@ namespace Popeye.Modules.Enemies.VFX
             public float _waitTime;
         }
 
+        [System.Serializable]
+        public class VFXInterpolateData
+        {
+            public float _startScale;
+            public float _endScale;
+            public AnimationCurve _scaleFunction;
+            public float _fadeOutDelay;
+            public float _fadeOutTime;
+            public AnimationCurve _fadeOutFunction;
+            public float _totalTime => _fadeOutDelay + _fadeOutTime;
+        }
+
+        [Header("CIRCLE")]
+        public VFXInterpolateData _circleInterpolateData;
+
+        [Header("PARTICLES")]
+        //saber a quina pool referirse??
+        
+        [Header("MATERIAL BLINK")]
         public List<MaterialFlash> _flashSequence = new();
     }
 }
