92fdc4c15a0e88259fe9a72a11d44b63f195f0c9
Eric <eric.ruiz@enti.cat>
2024-04-02 15:09:53 +0200

    Enemy OnHit and Death rework particles + camera shakes and hitstops

--- Assets/Project/Modules/Enemies/EnemyVFX/Scripts/EnemyVisuals.cs
+++ Assets/Project/Modules/Enemies/EnemyVFX/Scripts/EnemyVisuals.cs
@@ -5,10 +5,12 @@ using Cysharp.Threading.Tasks;
 using DG.Tweening;
 using Popeye.Core.Services.GameReferences;
 using Popeye.Core.Services.ServiceLocator;
+using Popeye.Modules.Camera.CameraShake;
 using Popeye.Modules.CombatSystem;
 using Popeye.Modules.VFX.ParticleFactories;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Popeye.Modules.Enemies.VFX
 {
@@ -22,11 +24,15 @@ namespace Popeye.Modules.Enemies.VFX
         }
 
         [SerializeField] private GeneralEnemyVFXConfig _visualConfig;
+        [SerializeField] private Transform _bleedingPoint;
+        [SerializeField] private Transform _visualCenter;
+        [SerializeField] private float _enemyWidth = 1.0f;
 
         [SerializeField] [Tooltip("First mesh on the list will be the one that gets 'hurt'")]
         private List<OriginalMeshData> _originalMeshDatas = new();
 
         private IParticleFactory _particleFactory;
+        private ICameraShaker _cameraShaker;
 
         private void Awake()
         {
@@ -36,15 +42,17 @@ namespace Popeye.Modules.Enemies.VFX
             }
         }
 
-        public void Configure(IParticleFactory particleFactory)
+        public void Configure(IParticleFactory particleFactory, ICameraShaker cameraShaker)
         {
             _particleFactory = particleFactory;
+            _cameraShaker = cameraShaker;
             _originalMeshDatas[0]._mesh.material.SetFloat("_Health", 1.0f);
         }
 
         public virtual void PlayHitEffects(float healthCoef01, DamageHit damageHit)
         {
             _originalMeshDatas[0]._mesh.material.SetFloat("_Health", healthCoef01);
+            _cameraShaker.PlayShake(_visualConfig.OnHitShakeConfig);
 
             ParticlesHitEffect(damageHit);
             FlashHitEffect().Forget();
@@ -53,6 +61,9 @@ namespace Popeye.Modules.Enemies.VFX
         public virtual void PlayDeathEffects(DamageHit damageHit)
         {
             ParticlesHitEffect(damageHit);
+
+            _cameraShaker.PlayShake(_visualConfig.DeathShakeConfig);
+            _particleFactory.Create(_visualConfig.DeathParticles, transform.position, quaternion.identity);
         }
 
         private void ParticlesHitEffect(DamageHit damageHit)
@@ -62,10 +73,23 @@ namespace Popeye.Modules.Enemies.VFX
             //get the     normal    of the contact poiint
             
             Transform player = ServiceLocator.Instance.GetService<IGameReferences>().GetPlayerTargetForEnemies();
-            Vector3 spawnPos = transform.position + (player.position - transform.position).normalized * 1.25f; //x.xf serves as enemy width
+            Vector3 spawnPos = transform.position + (player.position - transform.position).normalized * _enemyWidth;
+            Vector3 behindSpawnPos = transform.position - (player.position - transform.position).normalized * _enemyWidth;
             
-            _particleFactory.Create(_visualConfig.SplatterParticleType, spawnPos, quaternion.identity).LookAt(player);
-            _particleFactory.Create(_visualConfig.WaveParticleType, spawnPos, quaternion.identity).LookAt(player);
+            //_particleFactory.Create(_visualConfig.SplatterParticleType, spawnPos, quaternion.identity).LookAt(player);
+            //_particleFactory.Create(_visualConfig.WaveParticleType, spawnPos, quaternion.identity).LookAt(player);
+            
+            //Transform bloodHitDirectionalParticles = _particleFactory.Create(_visualConfig.BloodHitDirectionalParticles, _bleedingPoint.position, quaternion.identity);
+            //bloodHitDirectionalParticles.LookAt(player);
+            //bloodHitDirectionalParticles.RotateAround(bloodHitDirectionalParticles.position, bloodHitDirectionalParticles.up, 180.0f);
+            //bloodHitDirectionalParticles.SetParent(transform);
+            
+            Transform bloodHitSplashParticles = _particleFactory.Create(_visualConfig.BloodHitSplashParticles, _visualCenter.position, quaternion.identity);
+            //bloodHitSplashParticles.SetParent(transform);
+            
+            Transform bloodDripParticles = _particleFactory.Create(_visualConfig.BloodDripParticles, _bleedingPoint.position, quaternion.identity);
+            bloodDripParticles.LookAt(player);
+            bloodDripParticles.RotateAround(bloodDripParticles.position, bloodDripParticles.up, 180.0f);
         }
 
         private async UniTaskVoid FlashHitEffect()
