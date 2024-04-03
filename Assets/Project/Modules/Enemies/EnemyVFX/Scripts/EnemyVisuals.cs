32cee5fd58d09b193456fedd2585b84c9e476c7e
Eric <eric.ruiz@enti.cat>
2024-04-03 11:45:39 +0200

    Small soul rework and death amplified

--- Assets/Project/Modules/Enemies/EnemyVFX/Scripts/EnemyVisuals.cs
+++ Assets/Project/Modules/Enemies/EnemyVFX/Scripts/EnemyVisuals.cs
@@ -24,9 +24,7 @@ namespace Popeye.Modules.Enemies.VFX
         }
 
         [SerializeField] private GeneralEnemyVFXConfig _visualConfig;
-        [SerializeField] private Transform _bleedingPoint;
         [SerializeField] private Transform _visualCenter;
-        [SerializeField] private float _enemyWidth = 1.0f;
 
         [SerializeField] [Tooltip("First mesh on the list will be the one that gets 'hurt'")]
         private List<OriginalMeshData> _originalMeshDatas = new();
@@ -68,26 +66,11 @@ namespace Popeye.Modules.Enemies.VFX
 
         private void ParticlesHitEffect(DamageHit damageHit)
         {
-            //TODO: FIX these two things
-            //get the    position   of the contact point
-            //get the     normal    of the contact poiint
-            
             Transform player = ServiceLocator.Instance.GetService<IGameReferences>().GetPlayerTargetForEnemies();
-            Vector3 spawnPos = transform.position + (player.position - transform.position).normalized * _enemyWidth;
-            Vector3 behindSpawnPos = transform.position - (player.position - transform.position).normalized * _enemyWidth;
             
-            //_particleFactory.Create(_visualConfig.SplatterParticleType, spawnPos, quaternion.identity).LookAt(player);
-            //_particleFactory.Create(_visualConfig.WaveParticleType, spawnPos, quaternion.identity).LookAt(player);
+            _particleFactory.Create(_visualConfig.BloodHitSplashParticles, _visualCenter.position, quaternion.identity);
             
-            //Transform bloodHitDirectionalParticles = _particleFactory.Create(_visualConfig.BloodHitDirectionalParticles, _bleedingPoint.position, quaternion.identity);
-            //bloodHitDirectionalParticles.LookAt(player);
-            //bloodHitDirectionalParticles.RotateAround(bloodHitDirectionalParticles.position, bloodHitDirectionalParticles.up, 180.0f);
-            //bloodHitDirectionalParticles.SetParent(transform);
-            
-            Transform bloodHitSplashParticles = _particleFactory.Create(_visualConfig.BloodHitSplashParticles, _visualCenter.position, quaternion.identity);
-            //bloodHitSplashParticles.SetParent(transform);
-            
-            Transform bloodDripParticles = _particleFactory.Create(_visualConfig.BloodDripParticles, _bleedingPoint.position, quaternion.identity);
+            Transform bloodDripParticles = _particleFactory.Create(_visualConfig.BloodDripParticles, _visualCenter.position, quaternion.identity);
             bloodDripParticles.LookAt(player);
             bloodDripParticles.RotateAround(bloodDripParticles.position, bloodDripParticles.up, 180.0f);
         }
