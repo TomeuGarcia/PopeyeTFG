19ab7850576430b414d2f72b8e79669a3b662e25
TomeuGarcia <tomeu.garcia@enti.cat>
2024-04-15 10:13:32 +0200

    Update Hazards factory framework

---
+++ Assets/Project/Modules/Enemies/Hazards/Scripts/FlatStraightProjectile/FlatStraightProjectile.cs
@@ -0,0 +1,23 @@
+using Popeye.Core.Pool;
+using Popeye.Modules.VFX.ParticleFactories;
+
+namespace Popeye.Modules.Enemies.Hazards
+{
+    public class FlatStraightProjectile : RecyclableObject
+    {
+        internal override void Init()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        internal override void Release()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Configure(IParticleFactory particleFactory)
+        {
+            
+        }
+    }
+}
--- Assets/Project/Modules/Enemies/Hazards/Scripts/IHazardFactory.cs
+++ Assets/Project/Modules/Enemies/Hazards/Scripts/IHazardFactory.cs
@@ -4,8 +4,9 @@ namespace Popeye.Modules.Enemies.Hazards
 {
     public interface IHazardFactory
     {
-        ParabolicProjectile CreateParabolicProjectile(Transform origin,Transform targetPosition);
-
         AreaDamageOverTime CreateDamageArea(Vector3 position, Quaternion rotation);
+        ParabolicProjectile CreateParabolicProjectile(Transform origin,Transform targetPosition);
+        FlatStraightProjectile CreateFlatStraightProjectile(Vector3 position, Quaternion rotation);
+        Explosion CreateExplosion(Vector3 position, Quaternion rotation, ExplosionSize size);
     }
 }
