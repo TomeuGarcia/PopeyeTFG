e98692db42d432d02b0c39858841c9b9ddd4dfb6
Eric <eric.ruiz@enti.cat>
2024-04-29 22:45:29 +0200

    Texture, emmissive and implementation

--- Assets/Project/Modules/Enemies/Hazards/Scripts/HazardDispenser/View/HazardDispenserView.cs
+++ Assets/Project/Modules/Enemies/Hazards/Scripts/HazardDispenser/View/HazardDispenserView.cs
@@ -1,6 +1,10 @@
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
+using Popeye.Core.Services.ServiceLocator;
+using Popeye.Modules.VFX.Generic;
+using Popeye.Modules.VFX.ParticleFactories;
 using Project.Scripts.TweenExtensions;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace Popeye.Modules.Enemies.Hazards
@@ -9,36 +13,52 @@ namespace Popeye.Modules.Enemies.Hazards
     {
         [Header("COMPONENTS")]
         [SerializeField] private Transform _spitterHolder;
+        [SerializeField] private Transform _chargePosition;
+        [SerializeField] private SkinnedMeshRenderer _meshRenderer;
+        private Material _material;
         private HazardDispenserViewConfig _config;
+        private IParticleFactory _particleFactory;
         
         public void Configure(HazardDispenserViewConfig config)
         {
             _config = config;
+            _particleFactory = ServiceLocator.Instance.GetService<IParticleFactory>();
+            _material = _meshRenderer.material;
         }
 
         public void PlayPrepareDispensingAnimation(float duration)
         {
+            _particleFactory.Create(_config.ChargeParticleType, Vector3.zero, quaternion.identity, _chargePosition);
+            
+            /*
             _spitterHolder.DOComplete();
             _spitterHolder.DOBlendableLocalRotateBy(_config.PrepareRotation.Value, duration)
                 .SetEase(_config.PrepareRotation.Ease);
             _spitterHolder.DOScale(_config.PrepareScale.Value, duration)
                 .SetEase(_config.PrepareScale.Ease);
+            */
         }
 
         public void PlayDispenseAnimation()
         {
+            MaterialInterpolator.ApplyInterpolations(_material, _config.ReadyActivate).Forget();
+            
+            /*
             _spitterHolder.DOComplete();
             _spitterHolder.DOLocalRotateQuaternion(Quaternion.identity, _config.DispenseScalePunch.Duration)
                 .SetEase(Ease.InOutSine);
             _spitterHolder.PunchScale(_config.DispenseScalePunch);
+            */
         }
 
         public async UniTask PlayReadyToDispenseAnimation()
         {
+            /*
             _spitterHolder.PunchRotation(_config.ReadyRotationPunch);
             await _spitterHolder.DOScale(Vector3.one, _config.ReadyRotationPunch.Duration)
                 .SetEase(Ease.InOutSine)
                 .AsyncWaitForCompletion();
+            */
         }
     }
 }
--- Assets/Project/Modules/Enemies/Hazards/Scripts/HazardDispenser/View/HazardDispenserViewConfig.cs
+++ Assets/Project/Modules/Enemies/Hazards/Scripts/HazardDispenser/View/HazardDispenserViewConfig.cs
@@ -1,3 +1,5 @@
+using Popeye.Modules.VFX.Generic;
+using Popeye.Modules.VFX.Generic.MaterialInterpolationConfiguration;
 using Project.Scripts.TweenExtensions;
 using UnityEngine;
 
@@ -6,6 +8,16 @@ namespace Popeye.Modules.Enemies.Hazards
     [System.Serializable]
     public class HazardDispenserViewConfig
     {
+        [Header("CHARGE")]
+        [SerializeField] private ParticleTypes _chargeParticleType;
+        
+        [Header("READY")]
+        [SerializeField] private MaterialFloatInterpolationConfig[] _readyInterpolations;
+        
+        public ParticleTypes ChargeParticleType => _chargeParticleType;
+        public MaterialFloatInterpolationConfig[] ReadyActivate => _readyInterpolations;
+        
+        //TODO - Delete old
         [Header("PREPARE")]
         [SerializeField] private TweenConfig _prepareRotation;
         [SerializeField] private TweenConfig _prepareScale;
