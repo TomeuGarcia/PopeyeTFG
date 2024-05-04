6bfa88abd3434557848acbb02c419cc500f232fe
TomeuGarcia <tomeu.garcia@enti.cat>
2024-05-04 20:45:40 +0200

    Add Loading text when loading scenes

--- Assets/Project/General/Scripts/Core/Scenes/Loading/ScreenFade/CanvasSceneTransitionFader.cs
+++ Assets/Project/General/Scripts/Core/Scenes/Loading/ScreenFade/CanvasSceneTransitionFader.cs
@@ -12,6 +12,7 @@ namespace Popeye.Scripts.Core.Scenes
     {
         [Header("COMPONENTS")]
         [SerializeField] private CanvasGroup _fadeGroup;
+        [SerializeField] private CanvasGroup _textFadeGroup;
 
         [Header("CONFIGURATION")] 
         [Expandable] [SerializeField] private SceneTransitionScreenFaderConfig _config;
@@ -44,11 +45,13 @@ namespace Popeye.Scripts.Core.Scenes
         {
             _isFading = true;
             
-            
-            _fadeGroup.Fade(_config.FadeIn);
-    
-            
             float timeBeforeLoading = Time.time;
+
+            await _fadeGroup.Fade(_config.FadeIn)
+                .AsyncWaitForCompletion();
+    
+            _textFadeGroup.Fade(_config.LoadingTextFadeIn);
+            
 
             for (int i = 0; i < _sceneFinishedLoadingAwaits.Count; ++i)
             {
@@ -62,7 +65,7 @@ namespace Popeye.Scripts.Core.Scenes
             
             await UniTask.Delay(TimeSpan.FromSeconds(remainingFadedInTime));
 
-            
+            _textFadeGroup.Fade(_config.LoadingTextFadeOut);
             await _fadeGroup.Fade(_config.FadeOut).AsyncWaitForCompletion();
 
             _isFading = false;
