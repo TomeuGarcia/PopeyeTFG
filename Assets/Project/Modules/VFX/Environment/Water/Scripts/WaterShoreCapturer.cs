5f3caf96ccedf22e1ca6bf724c81633c67152e85
Eric <eric.ruiz@enti.cat>
2024-05-06 10:04:58 +0200

    Water fixed

--- Assets/Project/Modules/VFX/Environment/Water/Scripts/WaterShoreCapturer.cs
+++ Assets/Project/Modules/VFX/Environment/Water/Scripts/WaterShoreCapturer.cs
@@ -2,25 +2,39 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
+using Popeye.Core.Services.EventSystem;
+using Popeye.Core.Services.ServiceLocator;
+using Popeye.Modules.GameState;
 using UnityEngine;
 
 public class WaterShoreCapturer : MonoBehaviour
 {
     [SerializeField] private Camera _camera;
-    [SerializeField] private Transform _shoreMeshes;
-    
+    [SerializeField] private MeshRenderer _waterMeshRenderer;
+    private IEventSystemService _eventSystemService;
+
     private void Start()
     {
-        DeactvateCamera();
+        _eventSystemService = ServiceLocator.Instance.GetService<IEventSystemService>();
+        _eventSystemService.Subscribe<IGameStateEventsDispatcher.OnFinishLoadingScenes>(OnFinishLoadingSceneEvent);
     }
 
-    public async UniTask DeactvateCamera()
+    private void OnFinishLoadingSceneEvent(IGameStateEventsDispatcher.OnFinishLoadingScenes eventData)
     {
-        await UniTask.Delay(TimeSpan.FromSeconds(0.1f));
+        StartCoroutine(LoadedScene());
+    }
+
+    private IEnumerator LoadedScene()
+    {
+        yield return new WaitForSeconds(0.2f);
         
         _camera.gameObject.SetActive(false);
-        _shoreMeshes.gameObject.SetActive(false);
         
-        GameObject.Find("WaterPlane").GetComponent<MeshRenderer>().material.SetVector("_ShoreTextureOffset", new Vector4(_camera.gameObject.transform.position.x, _camera.gameObject.transform.position.z, 0.0f, 0.0f));
+        _waterMeshRenderer.material.SetVector("_ShoreTextureOffset", new Vector4(_camera.gameObject.transform.position.x, _camera.gameObject.transform.position.z, 0.0f, 0.0f));
+    }
+    
+    private void OnDestroy()
+    {
+        _eventSystemService.Unsubscribe<IGameStateEventsDispatcher.OnFinishLoadingScenes>(OnFinishLoadingSceneEvent);
     }
 }
