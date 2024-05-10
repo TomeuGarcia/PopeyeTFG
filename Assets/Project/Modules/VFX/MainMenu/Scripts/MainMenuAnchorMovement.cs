29de9308c2d48b13c3d3f9faa60ed8f5a311911d
Eric <eric.ruiz@enti.cat>
2024-05-10 04:26:53 +0200

    requestea aquest canvi 🖕

--- Assets/Project/Modules/VFX/MainMenu/Scripts/MainMenuAnchorMovement.cs
+++ Assets/Project/Modules/VFX/MainMenu/Scripts/MainMenuAnchorMovement.cs
@@ -16,13 +16,6 @@ public class MainMenuAnchorMovement : MonoBehaviour
         StartCoroutine(DoMove(Vector3.up, 0.25f));
         StartCoroutine(DoRotate(Vector3.up, 10.0f));
         StartCoroutine(DoRotate(Vector3.right, 5.0f));
-
-
-        //StartCoroutine(BlendableMove(Vector3.up, 0.5f));
-        //StartCoroutine(BlendableRotate(Vector3.up, 30.0f));
-        //yield return new WaitForSeconds(0.1f);
-        //StartCoroutine(BlendableMove(Vector3.right, 30.0f));
-        //StartCoroutine(BlendableRotate());
     }
 
     private IEnumerator DoMove(Vector3 axis, float strength)
@@ -44,30 +37,6 @@ public class MainMenuAnchorMovement : MonoBehaviour
             float randomStrength = Random.Range(-strength, strength);
             transform.DOBlendablePunchRotation(axis * randomStrength, randomTime, 0, 0).SetEase(Ease.InOutSine);
             yield return new WaitForSeconds(randomTime);
-        }
-    }
-
-    private IEnumerator BlendableMove(Vector3 axis, float strength)
-    {
-        while (true)
-        {
-            float randomTime = Random.Range(_minTime, _maxTime);
-            float randomMovement = Random.Range(-strength, strength);
-            yield return new WaitForSeconds(randomTime);
-
-            transform.DOBlendableLocalMoveBy(Vector3.up * randomMovement, randomTime);
-        }
-    }
-
-    private IEnumerator BlendableRotate(Vector3 axis, float strength)
-    {
-        while (true)
-        {
-            float randomTime = Random.Range(_minTime, _maxTime);
-            float randomMovement = Random.Range(-strength, strength);
-            yield return new WaitForSeconds(randomTime);
-
-            transform.DOBlendableLocalRotateBy(Vector3.up * randomMovement, randomTime);
         }
     }
 }
